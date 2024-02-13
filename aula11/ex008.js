eec9d4e6f885d5bd8437a98fdfb98affc8fb8a19
LaercioMatheus <154562692+LaercioMatheus@users.noreply.github.com>
2024-02-13 16:35:38 -0300

    Avançando nos estudos em JS
    Comecei a usar o console do VS para praticar os scripts.

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 70.5
+console.log (`A velocidade do seu carro é de ${vel}km/h`)
+
+if (vel > 60) {
+    console.log ("Você ultrapassou o limite de velocidade, MULTADO!")
+}// Condição simples
+
+console.log (`Dirija sempre com o cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = "Fraça"
+console.log(`Vivendo em ${pais}:`)
+if (pais == "Brasil") {
+    console.log ("Você é Brasileira")
+} else {
+    console.log ("Você é Estrangeira")
+}
+/*A CONDIÇÃO PODE SER AO CONTRARIA, NO IF SENDO != DIFERENTE*/
