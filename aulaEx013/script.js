ca9222a17debc5d318923a87f90dcb4a399eac51
LaercioMatheus <154562692+LaercioMatheus@users.noreply.github.com>
2024-02-14 20:49:10 -0300

    Completei o meu código.
    Dei continuidade para completar os códigos para as interações

--- aulaEx013/script.js
+++ aulaEx013/script.js
@@ -1,19 +1,29 @@
+/*Pegando os dados das tags do html e colocando dentro das variáveis*/
 function carregar() {
     var msg = window.document.getElementById ('msg')
     var img = window.document.getElementById ('imagem')
+    /*pegando a hora do sistema e colocando na variável */
     var data = new Date()
-    var hora = data.getHours()
+    var hora = data.getHours()//pegando a função de hora do JS
+    //mostrando o horário do sistema pela tag msg do html
+
     msg.innerHTML = `Agora são ${hora} horas.`
 
-    var hora = 1
+    //Condição para mostrar a imagem de acordo com o horário do sistema
     if (hora >= 0 && hora < 12) {
+        //carregando a imagem da manhã
         img.src = 'img/manha.png'
+        document.body.style.background = '#a5864d'
         //Bom dia
-    } else if (hora <= 18) {
+    } else if (hora >= 12 && hora <= 18) {
+        //carregando a imagem de tarde
         img.src = 'img/tarde.png'
+        document.body.style.background = '#497a28'
         //Boa tarde
     } else {
+        //carregando a imagem de noite
         img.src = 'img/noite.png'
+        document.body.style.background = '#2e333d'
         //Boa noite
     }
 }
